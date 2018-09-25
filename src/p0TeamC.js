750f199a364bf2bb6fad2d2ed6be714f1c3ee0da
John-OMeara <C00206417@itcarlow.ie>
2018-09-25 12:55:26 +0100

    First commit

---
+++ src/p0TeamC.js
@@ -0,0 +1,6 @@
+/*! p0TeamC v0.0.0 - MIT license */
+'use strict';
+
+var p0TeamC = function () {
+  // your code goes here
+}
---
+++ test/p0TeamC.js
@@ -0,0 +1,20 @@
+/* global p0TeamC, describe, it, expect, should */
+
+describe('p0TeamC()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(p0TeamC).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
