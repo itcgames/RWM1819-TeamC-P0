a34378a241cb0c6d801541e09f4c29063c5a9dd0
C00204864 <C00204864@itcarlow.ie>
2018-09-25 16:31:06 +0100

    Integrated three functions fully (I think running total may need modification)

--- src/p0TeamC.js
+++ src/p0TeamC.js
@@ -1,6 +1,12 @@
 /*! p0TeamC v0.0.0 - MIT license */
 'use strict';
 
-var p0TeamC = function () {
-  // your code goes here
+var p0TeamC = function (sequence) {
+  if(sequence.constructor !== Array) {
+    return undefined;
+  }
+  for(var i = 0; i < sequence.length; ++i) {
+    sequence[i] = running_total(Modulo_3(Cube(sequence[i])));
+  }
+  return sequence;
 }
